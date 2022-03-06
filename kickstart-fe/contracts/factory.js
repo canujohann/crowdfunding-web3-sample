b61a1507d3a7b6debd84f420fdc08e8de8a12697
johann canu <johann.canu@axa.co.jp>
2022-03-06 20:52:42 +0900

    move information to env variables

--- kickstart-fe/contracts/factory.js
+++ kickstart-fe/contracts/factory.js
@@ -1,8 +1,11 @@
 import web3 from "./web3";
 import CampaignFactory from "../../build/contracts/CampaignFactory.json";
 
-const FACTORY_ADDRESS = "0x837Fa1D91c122C616d7063F9B3B77a1D60b808B3";
+const instance = new web3.eth.Contract(
+  CampaignFactory.abi,
+  process.env.NEXT_PUBLIC_FACTORY_ADDRESS
+);
 
-const instance = new web3.eth.Contract(CampaignFactory.abi, FACTORY_ADDRESS);
+console.log(`factory address: ${process.env.NEXT_PUBLIC_FACTORY_ADDRESS}`);
 
 export default instance;
--- kickstart-fe/contracts/web3.js
+++ kickstart-fe/contracts/web3.js
@@ -1,5 +1,4 @@
 import Web3 from "web3";
-import { NODE_ADDRESS } from "./utils";
 
 let web3;
 
@@ -9,7 +8,9 @@ if (typeof window !== "undefined" && typeof window.ethereum !== "undefined") {
   web3 = new Web3(window.ethereum);
 } else {
   // We are on the server *OR* the user is not running metamask
-  const provider = new Web3.providers.HttpProvider(NODE_ADDRESS);
+  const provider = new Web3.providers.HttpProvider(
+    process.env.NEXT_PUBLIC_NODE_ADDRESS
+  );
   web3 = new Web3(provider);
 }
 
