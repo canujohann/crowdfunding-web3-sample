a02bebf63cf80f2be744351f3a587438b1dc0274
johann canu <johann.canu@axa.co.jp>
2022-03-06 20:52:21 +0900

    prettier fix

--- kickstart-fe/pages/index.js
+++ kickstart-fe/pages/index.js
@@ -1,15 +1,15 @@
-import React, { Component } from 'react';
-import { Card, Button } from 'semantic-ui-react';
-import factory from '../contracts/factory';
-import Layout from '../components/Layout';
-import { Link } from '../routes';
+import React, { Component } from "react";
+import { Card, Button } from "semantic-ui-react";
+import factory from "../contracts/factory";
+import Layout from "../components/Layout";
+import { Link } from "../routes";
 
 class CampaignIndex extends Component {
   static async getInitialProps() {
     const campaigns = await factory.methods.getDeployedCampaigns().call();
-
     return { campaigns };
   }
+
   renderCampaigns() {
     const items = this.props.campaigns.map((address) => {
       return {
