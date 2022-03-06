c8175e1c9a824879c56bd833826163a52b250f94
johann canu <johann.canu@axa.co.jp>
2022-03-06 20:50:35 +0900

    fix bug for manager adress of a new campaign

--- kickstart-fe/contracts/campaign.js
+++ kickstart-fe/contracts/campaign.js
@@ -1,8 +1,7 @@
 import web3 from "./web3";
 import Campaign from "../../build/contracts/Campaign.json";
-import { FACTORY_ADDRESS } from "./utils";
 
 const campaign = (address) => {
-  return new web3.eth.Contract(Campaign.abi, FACTORY_ADDRESS);
+  return new web3.eth.Contract(Campaign.abi, address);
 };
 export default campaign;
